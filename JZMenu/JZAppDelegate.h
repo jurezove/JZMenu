4e9b582714a13d29dd0bc87e44166e24835e8bb4
Jure Žove <jure.pase.kure@gmail.com>
2012-08-06 10:11:48 +0200

    Initial Commit

---
+++ JZMenu/JZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JZAppDelegate.h
+//  JZMenu
+//
+//  Created by Jure Zove on 6. 08. 12.
+//  Copyright (c) 2012 Jure Zove. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ JZMenu/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  JZMenu
+//
+//  Created by Jure Zove on 6. 08. 12.
+//  Copyright (c) 2012 Jure Zove. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JZAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JZAppDelegate class]));
+    }
+}
